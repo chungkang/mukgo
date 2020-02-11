5d31bf134a1aa224f45c0c2f927b30e083d1ef13
chungkang <chungkang.c@gmail.com>
2020-02-12 00:04:16 +0900

    basic UI

--- home/views.py
+++ home/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("먹방!")
+    msg = 'My Message'
+    return render(request, 'index.html', {'message': msg})
+
+
+def error(request):
+    raise Http404("Not Found")
